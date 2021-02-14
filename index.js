470d22f0d274cd841a1799465012a1fc9478414c
Tomi Toivio <tomi.toivio@ukuli.fi>
2021-02-14 16:57:27 +0200

    Menu
    Co-Authored-By: Tomi Toivio <tomi.toivio@ukuli.fi>
    Co-Authored-By: Ukuli Data <info@ukuli.fi>

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-const { app, BrowserWindow } = require('electron');      
-let window;      
-app.on('ready', () => {          
-    window = new BrowserWindow({            
-        width: 800,            
-        height: 600,            
-        webPreferences: {              
-            nodeIntegration: true            
-            }          
-            });          
-            window.loadFile('index.html');      
-            });
+      const { app, BrowserWindow, Menu } = require('electron');
+      const menu = require('./menu');
+      
+      let window;
+      
+      app.on('ready', () => {
+          window = new BrowserWindow({
+            width: 800,
+            height: 600,
+            webPreferences: {
+              nodeIntegration: true
+            }
+          });
+          window.loadFile('index.html');
+      });
+
+      Menu.setApplicationMenu(menu);
